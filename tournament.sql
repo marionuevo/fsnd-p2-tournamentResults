bb2939cffbb00bec42d03d31f9f552d528618b19
marionuevo <marionuevo@gmail.com>
2015-04-10 14:32:59 +0100

    Initial own work

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,18 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- creating the DATABASE
+CREATE DATABASE tournament;
 
+-- creating TABLES
+CREATE TABLE players (
+	id serial PRIMARY KEY,
+	name text
+);
+
+CREATE TABLE matches (
+	p1 integer,
+	p2 integer,
+	winner integer,
+	PRIMARY KEY (p1, p2)
+);
